256dad876ca722ee0e46bc16aa7dd475ea88e3b7
Rawankh7 <rawankhaldi@outlook.sa>
2025-10-16 10:21:08 +0300

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by rawan alkhaldi  on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
